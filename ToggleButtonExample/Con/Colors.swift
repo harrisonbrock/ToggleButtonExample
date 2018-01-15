b9f090d65702b30ef590d54d6f8b6908b3e09e40
Harrison Brock <harrison@harrisonbrock.com>
2018-01-15 15:23:02 -0500

    Start of example project

---
+++ ToggleButtonExample/Con/Colors.swift
@@ -0,0 +1,14 @@
+//
+//  Colors.swift
+//  ToggleButtonExample
+//
+//  Created by Harrison Brock on 1/15/18.
+//  Copyright © 2018 Harrison Brock. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Colors {
+    static let toggolButton = UIColor(red: 29.0/255.0, green: 161.0/255, blue: 242.0/255, alpha: 1.0)
+}
---
+++ ToggleButtonExample/UI Controls/HBToggleButton.swift
@@ -0,0 +1,55 @@
+//
+//  ToggleButton.swift
+//  ToggleButtonExample
+//
+//  Created by Harrison Brock on 1/15/18.
+//  Copyright © 2018 Harrison Brock. All rights reserved.
+//
+
+import UIKit
+
+class HBToggleButton: UIButton {
+
+    var isOn = false
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        initButton()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        initButton()
+    }
+    
+    func initButton() {
+        
+        layer.borderWidth = 2.0
+        layer.borderColor = Colors.toggolButton.cgColor
+        layer.cornerRadius = frame.size.height / 2
+        
+        setTitleColor(Colors.toggolButton, for: .normal)
+        addTarget(self, action: #selector(HBToggleButton.buttonPressed), for: .touchUpInside)
+    }
+    
+    @objc func buttonPressed() {
+        activateButton(bool: !isOn)
+    }
+    
+
+    func activateButton(bool: Bool) {
+        isOn = bool
+        setButtonValues(bool)
+    }
+    
+    fileprivate func setButtonValues(_ bool: Bool) {
+        
+        let color = bool ? Colors.toggolButton : .clear
+        let title = bool ? "On" : "Off"
+        let titleColor = bool ? . white : Colors.toggolButton
+        
+        setTitle(title, for: .normal)
+        setTitleColor(titleColor, for: .normal)
+        backgroundColor = color
+    }
+}
