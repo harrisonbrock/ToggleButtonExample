34270267f83472a7a0ee3d1199040d7a3a6cf2c5
Harrison Brock <harrison@harrisonbrock.com>
2018-01-15 14:16:39 -0500

    Initial Commit

---
+++ ToggleButtonExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ToggleButtonExample
+//
+//  Created by Harrison Brock on 1/15/18.
+//  Copyright © 2018 Harrison Brock. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
